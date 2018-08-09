587248d3119b0a034fab9e27a186865354fc27e0
Sebastian Dziadzio <sebastian.dziadzio@gmail.com>
2018-08-09 17:08:34 +0200

    Finish the resizing script

--- resize.py
+++ resize.py
@@ -15,23 +15,30 @@ def remove_transparency(im, bg_colour=(255, 255, 255)):
         return im
 
 
+SIZE = 200, 200
+dataset_path = 'dataset_sample'
+
 sc = smartcrop.SmartCrop()
 crop_options = smartcrop.DEFAULTS
-crop_options['width'] = 100
+crop_options['width'] = 100 
 crop_options['height'] = 100
 
-dataset_path = 'dataset_sample'
+
 files = glob(os.path.join(dataset_path, '*', '*.jpg'))
 other = glob(os.path.join(dataset_path, '*', '*.png'))
 files.extend(other)
 
-for image in files:
-    img = Image.open(image)
-    print(image)
+for path in files:
+    print(path)
+    name, extension = os.path.splitext(path)
+    img = Image.open(path)
     img = remove_transparency(img)
-    crops = sc.crop(img, crop_options)
+
+    crops = sc.crop(img.copy(), crop_options)
     best = crops['topCrop']
     x, y = best['x'], best['y'] 
     w, h = best['width'], best['height']
+
+    img.crop((x, y, x+w, y+h)).convert('RGB').resize(SIZE).save(name + '_cropped.jpg')
 
 print(files)
